f5d4558a6f4f5cae466fe87713c61b02af251f44
saramartignoni <martignoni@fbk.eu>
2017-07-14 11:31:08 +0200

    Rimossa opzione di ricerca per mappa e calendario

--- mobile/src/app/in-memory-data.service.ts
+++ mobile/src/app/in-memory-data.service.ts
@@ -2,17 +2,17 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const mainEvents = [
-      { id: 1,  title: 'Evento 1', description: 'Descrizione 1', image:'assets/icon/test1.jpg', tags:['party'], source:'comunetn', themes: ['cultura'], coordX: 46.06771, coordY: 11.14955, created: '20170705110001', eventDate: '20170705110001' },
-      { id: 2,  title: 'Evento 2', description: 'Descrizione 2', image:'assets/icon/test2.jpg', tags:['exhibition','party'], source:'comunetn', themes: ['cultura'], coordX: 46.09070, coordY: 11.10998, created: '20170705110002', eventDate: '20170705110001' },
-      { id: 3,  title: 'Evento 3c', description: 'Descrizione 3', image:'assets/icon/test3.jpg', tags:['match'], source:'asis', themes: ['sport'], coordX: 46.05382, coordY: 11.12668, created: '20170705110003', eventDate: '20170705110001' },
-      { id: 4,  title: 'Evento 4', description: 'Descrizione 4', image:'assets/icon/test4.jpg', tags:['music'], source:'socialstone', themes: ['cultura'], coordX: 46.05877, coordY: 11.13091, created: '20170705110004', eventDate: '20170705110001' },
+      { id: 1,  title: 'Evento 1', description: 'Descrizione 1', image:'assets/icon/test1.jpg', tags:['party'], source:'comunetn', themes: ['cultura'], coordX: 46.06771, coordY: 11.14955, created: '20170705110001', eventDate: '20170405110001' },
+      { id: 2,  title: 'Evento 2', description: 'Descrizione 2', image:'assets/icon/test2.jpg', tags:['exhibition','party'], source:'comunetn', themes: ['cultura'], coordX: 46.09070, coordY: 11.10998, created: '20170705110002', eventDate: '20270705110001' },
+      { id: 3,  title: 'Evento 3c', description: 'Descrizione 3', image:'assets/icon/test3.jpg', tags:['match'], source:'asis', themes: ['sport'], coordX: 46.05382, coordY: 11.12668, created: '20170705110003', eventDate: '20170719110001' },
+      { id: 4,  title: 'Evento 4', description: 'Descrizione 4', image:'assets/icon/test4.jpg', tags:['music'], source:'socialstone', themes: ['cultura'], coordX: 46.05877, coordY: 11.13091, created: '20170705110004', eventDate: '20140805110001' },
       { id: 5,  title: 'Evento 5', description: 'Descrizione 5', image:'assets/icon/test5.jpg', tags:['party'], source:'socialstone', themes: ['cultura'], coordX: 46.05877, coordY: 11.13091, created: '20170705110005', eventDate: '20160705110001' },
-      { id: 6,  title: 'Evento 6', description: 'Descrizione 6', image:'assets/icon/test6.jpg', tags:['race'], source:'comunetn', themes: ['sport'], coordX: 46.06937, coordY: 11.12123, created: '20170705110006', eventDate: '20170705110001' },
+      { id: 6,  title: 'Evento 6', description: 'Descrizione 6', image:'assets/icon/test6.jpg', tags:['race'], source:'comunetn', themes: ['sport'], coordX: 46.06937, coordY: 11.12123, created: '20170705110006', eventDate: '20170703110001' },
       { id: 7,  title: 'Evento 7', description: 'Descrizione 7', image:'assets/icon/test7.jpg', tags:['exhibition'], source:'comunetn', themes: ['cultura'], coordX: 46.06496, coordY: 11.12305, created: '20170705110007', eventDate: '20140705110001' },
       { id: 8,  title: 'Evento 8', description: 'Descrizione 8', image:'assets/icon/test8.jpg', tags:['music'], source:'donquijote', themes: ['cultura'], coordX: 46.07024, coordY: 11.12351, created: '20170705110008', eventDate: '20150705110001' },
       { id: 9,  title: 'Evento 9', description: 'Descrizione 9', image:'assets/icon/test9.jpg', tags:['party'], source:'donquijote', themes: ['cultura'], coordX: 46.07024, coordY: 11.12351, created: '20170705110009', eventDate: '20170705110001' },
       { id: 10,  title: 'Evento 10', description: 'Descrizione 10', image:'assets/icon/test10.jpg', tags:['match'], source:'comunetn', themes: ['sport'], coordX: 46.04100, coordY: 11.12334, created: '20170705110010', eventDate: '20130705110001' },
-    ];
+      ];
     return {mainEvents};
   }
 }
