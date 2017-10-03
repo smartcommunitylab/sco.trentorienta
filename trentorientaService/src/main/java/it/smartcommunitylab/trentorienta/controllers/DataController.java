e45b57dc72b40f17a442dcc7dbb79abc7c161bc2
Nawaz Khurshid <nawaz1981@gmail.com>
2017-10-03 14:04:34 +0200

    Fix for removal of unused import

--- trentorientaService/src/main/java/it/smartcommunitylab/trentorienta/controllers/DataController.java
+++ trentorientaService/src/main/java/it/smartcommunitylab/trentorienta/controllers/DataController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
