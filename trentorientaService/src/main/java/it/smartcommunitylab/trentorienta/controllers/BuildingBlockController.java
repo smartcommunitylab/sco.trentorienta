28ce1f2bda955b3f13b694036d6c51616c1e80b2
Nawaz Khurshid <nawaz1981@gmail.com>
2017-11-15 17:18:32 +0100

    Fix for usdl specification.

--- trentorientaService/src/main/java/it/smartcommunitylab/trentorienta/controllers/BuildingBlockController.java
+++ trentorientaService/src/main/java/it/smartcommunitylab/trentorienta/controllers/BuildingBlockController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.namespace.QName;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 
 import com.autentia.web.rest.wadl.builder.ApplicationBuilder;
 import com.autentia.web.rest.wadl.builder.impl.springframework.SpringWadlBuilderFactory;
@@ -42,14 +40,14 @@ public class BuildingBlockController {
 	private final SchemaBuilder schemaBuilder;
 	private USDLService usdlService;
 	private static final List<String> specs = Arrays.asList("xwadl", "wsdl", "usdl", "swagger");
-	
+
 	@PostConstruct
 	public void BuildingBlockController() {
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		InputStream is = classloader.getResourceAsStream("artifact-reference.yml");
 		usdlService = new USDLService(is);
 	}
-	
+
 	@Autowired
 	public BuildingBlockController(RequestMappingHandlerMapping handlerMapping) {
 		final SpringWadlBuilderFactory wadlBuilderFactory = new SpringWadlBuilderFactory(handlerMapping);
@@ -63,16 +61,16 @@ public class BuildingBlockController {
 	public List<String> getSpecList(HttpServletResponse response) {
 		return specs;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "spec/usdl", method = RequestMethod.GET, produces = { "application/rdf+xml" })
 	public String generateUSDL(HttpServletRequest request) {
 		return usdlService.generateBB();
 	}
-	
+
 	@RequestMapping(value = "spec/swagger", method = RequestMethod.GET)
 	public void generateSpec(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.sendRedirect( request.getContextPath()+ "/swagger-ui.html");
+		response.sendRedirect(request.getContextPath() + "/swagger-ui.html");
 	}
 
 	@ApiOperation(value = "spec/xwadl")
@@ -80,39 +78,31 @@ public class BuildingBlockController {
 	@RequestMapping(value = "spec/xwadl", method = RequestMethod.GET, produces = { "application/xml" })
 	public Application generateWadl(HttpServletRequest request) {
 		Application application = applicationBuilder.build("https://tn.smartcommunitylab.it");
-		
+
 		/**
 		 * its a bug of marketplace, it expect resources to be available inside
-		 * <resource path="<applicationName>">
-		 * 	<resource op1>
-		 * 	<request/>
-		 *  <response>
-		 *  	<representation mediaType="/"/>
-		 *  </response>
-		 *  </resource>
-		 *  <resource op2>
-		 *  </resource>
-		 * </resource>
+		 * <resource path="<applicationName>"> <resource op1>
+		 * <request/> <response> <representation mediaType="/"/> </response>
+		 * </resource> <resource op2> </resource> </resource>
 		 */
 		Resource trentOrienta = new Resource();
 		trentOrienta.setPath(request.getContextPath());
 
-		
-		for(Resource temp: application.getResources().get(0).getResource()) {
+		for (Resource temp : application.getResources().get(0).getResource()) {
 			if (temp.getPath().startsWith("/api/")) {
-				
+
 				Representation representation = new Representation();
 				representation.getOtherAttributes().put(QName.valueOf("mediaType"), "*/*");
-				
+
 				Method method = (Method) temp.getMethodOrResource().get(0);
 				method.getResponse().get(0).getRepresentation().add(representation);
 				trentOrienta.getAny().add(temp);
 			}
 		}
-		
+
 		application.getResources().get(0).getResource().clear();
 		application.getResources().get(0).getResource().add(trentOrienta);
-		
+
 		return application;
 	}
 
@@ -120,9 +110,9 @@ public class BuildingBlockController {
 	@ResponseBody
 	@RequestMapping(value = "spec/wsdl", method = RequestMethod.GET, produces = { "application/xml" })
 	public void generateWSDL(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.sendRedirect( request.getContextPath()+ "/soapservice/service/wsdl/service.wsdl");
+		response.sendRedirect(request.getContextPath() + "/soapservice/service/wsdl/service.wsdl");
 	}
-	
+
 	@ApiIgnore
 	@ResponseBody
 	@RequestMapping(value = "schema/{classTypeName}", method = RequestMethod.GET)
