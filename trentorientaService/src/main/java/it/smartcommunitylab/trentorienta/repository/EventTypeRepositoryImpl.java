2c508ef450b22c4a37364348022377b87f507ae0
Nawaz Khurshid <nawaz1981@gmail.com>
2018-01-16 16:26:54 +0100

    Fix for revert to previous version

--- trentorientaService/src/main/java/it/smartcommunitylab/trentorienta/repository/EventTypeRepositoryImpl.java
+++ trentorientaService/src/main/java/it/smartcommunitylab/trentorienta/repository/EventTypeRepositoryImpl.java
@@ -54,41 +54,30 @@ public class EventTypeRepositoryImpl implements EventTypeRepositoryCustom {
 		// log 'SearchEvent'
 		log("SearchEvent", "SearchEvent", UUID.randomUUID().toString());
 
-		List<Criteria> criteriaSource = new ArrayList<Criteria>();
-		List<Criteria> criteriaTheme = new ArrayList<Criteria>();
-		List<Criteria> criteriaTag = new ArrayList<Criteria>();
-	
-		Criteria criteriaTh = new Criteria();
+		List<Criteria> criteria = new ArrayList<Criteria>();
+		Criteria SearchCriteria = new Criteria();
+
 		if (themes != null) {
 			for (int i = 0; i < themes.length; i++) {
-				criteriaTheme.add(Criteria.where("themes").is(themes[i]));
+				criteria.add(Criteria.where("themes").is(themes[i]));
 			}
-			criteriaTh.orOperator(criteriaTheme.toArray(new Criteria[criteriaTheme.size()]));
-
 		}
-
-		Criteria criteriaS = new Criteria();
 		if (sources != null) {
 			for (int i = 0; i < sources.length; i++) {
-				criteriaSource.add(Criteria.where("source").is(sources[i]));
+				criteria.add(Criteria.where("source").is(sources[i]));
 			}
-			criteriaS.orOperator(criteriaSource.toArray(new Criteria[criteriaSource.size()]));
-
 		}
-
-		Criteria criteriaTa = new Criteria();
 		if (tags != null) {
 			for (int i = 0; i < tags.length; i++) {
 				if (tags[i].compareTo("null") != 0)
-					criteriaTag.add(Criteria.where("tags").in(tags[i]));
+					criteria.add(Criteria.where("tags").in(tags[i]));
 			}
-			criteriaTa.orOperator(criteriaTag.toArray(new Criteria[criteriaTag.size()]));
-
 		}
+		/*
+		 * if (fromDate != null) { criteria.add(Criteria.where("toTime").gte (
+		 * Long.parseLong(fromDate) )); }
+		 */
 
-		Criteria SearchCriteria = new Criteria().andOperator(criteriaTh, criteriaS, criteriaTa);
-
-	
 		Query query = new Query();
 
 		if (filter != null && filter != "") {
@@ -96,11 +85,14 @@ public class EventTypeRepositoryImpl implements EventTypeRepositoryCustom {
 			query = TextQuery.queryText(criteriaTesto).sortByScore();
 		}
 
-	
 		if (fromDate != null)
 			SearchCriteria.andOperator(Criteria.where("toTime").gte(fromDate.getTime()));
 
-		query.addCriteria(SearchCriteria);
+		if (criteria.size() > 0)
+			SearchCriteria.orOperator(criteria.toArray(new Criteria[criteria.size()]));
+
+		if (fromDate != null || criteria.size() > 0)
+			query.addCriteria(SearchCriteria);
 
 		System.out.println(query.toString());
 
